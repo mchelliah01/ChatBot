be6c3ef859fce635f31e7f3861ce0ae300288c41
Jonathan Dizon <jonathan@Jonathans-MacBook-Pro.local>
2024-04-12 15:28:28 -0700

    Integrated translation feature

--- pages/Assitant.py
+++ pages/Assitant.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import time
+import os
+import openai
 from openai import OpenAI
 
-client = OpenAI(api_key='apikey')
+from translate import translate
 
+openai.api_key = os.environ["OPENAI_API_KEY"]
+client = OpenAI()
 
 def thread(street, date_range):
     thread = client.beta.threads.create(
         messages=[
             {
                 "role": "user",
-                "content": f"Street: {street} || Date range:{date_range}"
+                "content": f"Street: {street} || Date range: {date_range}"
             }
         ]
     )
@@ -31,14 +35,33 @@ def thread(street, date_range):
     message_response = client.beta.threads.messages.list(thread.id)
     messages = message_response.data[0].content[0].text.value
     
-    print(messages)
     return messages
 
+# Streamlit
+st.markdown("# Page 2: Parking Advice 🚨")
+st.sidebar.markdown("# Page 2: Parking Advice 🚨")
+
+st.title("SFPD Incident Report Analyzer")
+
 with st.form(key = "chat"):
+
+    st.header("Street Name")
     street = st.text_input("Enter a street name within San Francisco: ") 
+    st.caption("Example: McAllister St")
+
+    st.header("Date Range")
     date_range = st.text_input("Enter a date, ranging from January 2018 to March 2024: ")
+    st.caption("Example: January 2024 to March 2024")
     
     submitted = st.form_submit_button("Submit")
+
+    if st.session_state['source_language'] != st.session_state['target_language']:
+        st.caption(f'Translating into {st.session_state['target_language']} from {st.session_state['source_language']}')
     
     if submitted:
-        st.write(thread(street, date_range))
+        if st.session_state['source_language'] != st.session_state['target_language']:
+            text = thread(street, date_range)
+            st.write(f'Translated into {st.session_state['target_language']}')
+            st.write(translate(text, st.session_state['source_language'], st.session_state['target_language']))
+        else:
+            st.write(thread(street, date_range))
--- pages/translate.py
+++ pages/translate.py
@@ -5,30 +5,29 @@ import streamlit as st
 import requests
 from openai import OpenAI
 
-st.markdown("# Page 3: Translation ❄️")
-st.sidebar.markdown("# Page 3: Translation ❄️")
+#st.markdown("# Page 1: Translation ❄️")
+#st.sidebar.markdown("# Page 1: Translation ❄️")
 
 # Create two radio buttons
-source_language = st.radio('Select Source language', ['English', 'French', 'German', 'Chinese'])
-target_language = st.radio('Select Target language', ['English', 'French', 'German', 'Chinese'])
+#source_language = st.radio('Select Source language', ['English', 'French', 'German', 'Chinese'])
+#target_language = st.radio('Select Target language', ['English', 'French', 'German', 'Chinese'])
 
 # Create a text input field
-text = st.text_input('Enter the text you want to translate: ')
+#text = st.text_input('Enter the text you want to translate: ')
 
 # Create a button
-if st.button('Submit'):
+#if st.button('Submit'):
     # Print the input from the text field and radio buttons
-    st.write(f'You entered: {text}')
-    st.write(f'Source language: {source_language}')
-    st.write(f'Target language: {target_language}')
+    #st.write(f'You entered: {text}')
+    #st.write(f'Source language: {source_language}')
+    #st.write(f'Target language: {target_language}')
 
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 client = OpenAI()
 
-
-def translate(text, source_language = "English", target_language = "Chinese"):
+def translate(text, source_language, target_language):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -49,4 +48,4 @@ def translate(text, source_language = "English", target_language = "Chinese"):
     
     return response.choices[0].message.content
 
-st.write(translate(text, source_language, target_language))
+#st.write(translate(text, source_language, target_language))
