67b68c00f8f6d0f2babcab2de5e05e32f3386d7d
Jonathan Dizon <dizon.jonathan.a@gmail.com>
2024-04-14 01:32:00 -0700

    Creation of assistant from within program

--- main_page.py
+++ main_page.py
@@ -6,6 +6,7 @@ st.sidebar.markdown("# Main page 🎈")
 message = "Welcome to the main page! 🎉\nPlease navigate the pages to explore some sample code."
 
 st.write(message)
+st.write("Note that opening the Assistant page will create a new assistant and upload a 24.6MB file to it.")
 
 source_language = st.radio('Select Source language', ['English', 'French', 'German', 'Chinese'])
 target_language = st.radio('Select Target language', ['English', 'French', 'German', 'Chinese'])
--- pages/Assistant.py
+++ pages/Assistant.py
@@ -8,6 +8,25 @@ from pages.translate import translate
 openai.api_key = os.environ["OPENAI_API_KEY"]
 client = OpenAI()
 
+file = client.files.create(
+  file=open("datasets\SF_20240414.csv", "rb"),
+  purpose='assistants'
+)
+
+assistant = client.beta.assistants.create(
+    name="Vehicle Theft Data Analysis",
+    instructions="\
+    You will receive a street and date range (from January 2018 to April 2024) for analysis.\n\
+    # 1. With the date range, check the number of incidents that occurred in that range and tell the user. Don't check the file if the date range is invalid. Immediately stop.\n\
+    # 2. With the street name, check the intersections. Pay attention under the incident_description column for the number of incidents.\n\
+    # 3. Tell the user the overall number of incidents in the city that occurred within that date range compared to the number of incidents that occurred that occurred within that date range for the given street.\n\
+    Keep it text only.\n\
+    Take a break before responding.",
+    tools=[{"type": "code_interpreter"}],
+    file_ids=[file.id],
+    model="gpt-3.5-turbo",
+    )
+
 def thread(street, date_range):
     thread = client.beta.threads.create(
         messages=[
@@ -20,7 +39,7 @@ def thread(street, date_range):
 
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
-        assistant_id="asst_br2JCiPSMrUesGqOkFyATP58"
+        assistant_id=assistant.id
     )
 
     while run.status != 'completed':
@@ -49,18 +68,18 @@ with st.form(key = "chat"):
     st.caption("Example: McAllister St")
 
     st.header("Date Range")
-    date_range = st.text_input("Enter a date, ranging from January 2018 to March 2024: ")
+    date_range = st.text_input("Enter a date, ranging from January 2018 to April 2024: ")
     st.caption("Example: January 2024 to March 2024")
     
     submitted = st.form_submit_button("Submit")
 
     if st.session_state['source_language'] != st.session_state['target_language']:
-        st.caption(f'Translating into {st.session_state['target_language']} from {st.session_state['source_language']}')
+        st.caption(f'Translating into {st.session_state["target_language"]} from {st.session_state["source_language"]}')
     
-    if submitted:
+    if submitted: # Create assistant only AFTER hitting submit?
         if st.session_state['source_language'] != st.session_state['target_language']:
             text = thread(street, date_range)
-            st.write(f'Translated into {st.session_state['target_language']}')
+            st.write(f"Translated into {st.session_state['target_language']}")
             st.write(translate(text, st.session_state['source_language'], st.session_state['target_language']))
         else:
             st.write(thread(street, date_range))
