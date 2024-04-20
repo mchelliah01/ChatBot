aa84bab64afc261961bdc86d6b40e83c3ae7ed26
Woojin Park <woojinpark.software@gmail.com>
2024-04-20 15:47:02 -0700

    :lipstick: Advice UI/UX update

--- pages/ParkAnalysis.py
+++ pages/ParkAnalysis.py
@@ -3,6 +3,7 @@ import openai
 import streamlit as st
 from openai import OpenAI
 from functions.translate import translate
+from datetime import datetime
 
 st.markdown("# Parking Advice 💬")
 st.sidebar.markdown("# Parking Advice 💬")
@@ -10,7 +11,7 @@ st.sidebar.markdown("# Parking Advice 💬")
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 client = OpenAI()
-# create a wrapper function
+
 def get_completion(prompt, model="gpt-3.5-turbo"):
    completion = client.chat.completions.create(
         model=model,
@@ -23,20 +24,21 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
    return completion.choices[0].message.content
 
-# create our streamlit app
 with st.form(key = "chat"):
-    prompt = st.text_input("Please enter a street name within San Francisco, current time, and/or surrounding environment:") 
-    
+    street_name = st.text_input("Please enter a street name within SF🌉:") 
+    date = st.date_input("Please select a date📅:")
+    time = st.time_input("Please select a time⏰:")
+    current_time = datetime.combine(date, time)
+    environment = st.text_input("Please describe the surrounding environment or/and weather🌦️:")
+
     submitted = st.form_submit_button("Submit")
 
-    if st.session_state['source_language'] != st.session_state['target_language']:
-        st.caption(f'Translating into {st.session_state["target_language"]} from {st.session_state["source_language"]}')
-
     if submitted:
-        
+        prompt = f"Street Name: {street_name}, Current Time: {current_time}, Environment: {environment}"
+
         if st.session_state['source_language'] != st.session_state['target_language']:
+            st.caption(f'Translating into {st.session_state["target_language"]} from {st.session_state["source_language"]}')
             text = get_completion(prompt)
-            st.write("Translated into {st.session_state['target_language']}")
             st.write(translate(text, st.session_state['source_language'], st.session_state['target_language']))
         else:
             st.write(get_completion(prompt))
